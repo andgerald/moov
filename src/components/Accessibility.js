020c41a75ec3d6519aec0976e69e464323559257
Corina Varas <56646549+CorinaVaras@users.noreply.github.com>
2020-03-18 09:44:00 -0300

    estructura general (#2)

---
+++ src/components/Accessibility.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Accessibility extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <h1>Componente de Accessibility</h1>
+            </div>
+        );
+    }
+}
+
+export default Accessibility;
