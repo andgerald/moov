6090bb7374860c7e7c4070807cf22000a8801bf1
MonicaBartha <barthamonica@gmail.com>
2020-03-21 15:13:33 -0300

    commit antes de pull, sin cambios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../App.css';
+import {DisplayMap} from './DisplayMap';
 
 class Home extends Component {
     render() {
         return (
             <div className='App'>
                 <h1>Componente Home</h1>
+                <DisplayMap/>
             </div>
         );
     }
