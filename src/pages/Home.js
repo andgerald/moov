da9227250bc3c2d08f627bde87a9f321bfa0565e
MonicaBartha <barthamonica@gmail.com>
2020-03-26 21:31:14 -0300

    botones/columnas de la pagina Home, responsive navigation y merge con el commit de Corina

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
-import NavBar from '../components/MyNavBar'
+import {Container, Row, Col} from 'react-bootstrap';
+import '../App.css';
 
 class Home extends Component {
     render() {
         return (
-            <div>
+            <div className="homeCategories">
                 <p>Vista Componente Home</p>
+                <Container>
+                    <Row>
+                        <Col>
+                          <div className="categoryBox">
+                            <p>Mapa</p>
+                          </div>
+                        </Col>
+                        <Col>
+                            <div className="categoryBox">
+                                <p>Servicios</p>    
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                          <div className="categoryBox">
+                             <p>Favoritos</p>    
+                          </div>
+                        </Col>
+                        <Col>
+                          <div className="categoryBox">
+                              <p>Transporte</p>
+                          </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
