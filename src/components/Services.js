220c463944604e6cdca014bbee348502043556a0
Corina Varas <56646549+CorinaVaras@users.noreply.github.com>
2020-03-29 20:00:13 -0300

    Vista servicio (#9)
    * estructura general
    
    * Create an instance of the map, implements default interactions for zoom and
    
    * reorganizacion de carpetas para los componentes, vista welcome, vista maps
    
    * view service
    
    * fix menu nav bar
    
    * fix routing
    
    * funcionalidad componente home
    
    * data json y componente listServices
    
    * z index
    
    Co-authored-by: MonicaBartha <56646294+MonicaBartha@users.noreply.github.com>

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,42 +1,46 @@
-import React, { Component } from 'react';
-import '../assets/css/services.css';
-import ListServices from './ListServices';
+import React, { Component } from "react";
+import "../assets/css/services.css";
+import ListServices from "./ListServices";
 
 class Category extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePage: 0,
+      typePage: ''
+    };
+  }
 
-    
+  changeState = (numero, type) => {
+    this.setState({
+      activePage: numero,
+      typePage: type
+      
+    });
+  };
 
-    render() {
+
+  render() {
+    if (this.state.activePage === 0) {
         return (
-
-            <ListServices/>
-            // <div>
-            //     <p className='titleServices'>Services</p>
-            //     <div className='sectionServices'>
-            //         <div className='itemServices'>Restaurantes</div>
-            //         <div className='itemServices'>Hoteles</div>
-            //         <div className='itemServices'>Museos</div>
-            //         <div className='itemServices'>Turismo Aventura</div>
-            //         <div className='itemServices'>Playas y Balnearios</div>
-            //         <div className='itemServices'>Parques</div>
-            //         <div className='itemServices'>Otros</div>
-            //     </div>
-            // </div>
-
-            <div>
-                <p className='titleServices'>Services</p>
-                <div className='sectionServices'>
-                    <div className='itemServices'>Restaurantes</div>
-                    <div className='itemServices'>Hoteles</div>
-                    <div className='itemServices'>Museos</div>
-                    <div className='itemServices'>Turismo Aventura</div>
-                    <div className='itemServices'>Playas y Balnearios</div>
-                    <div className='itemServices'>Parques</div>
-                    <div className='itemServices'>Otros</div>
-                </div>
-            </div>
+          <div>
+            <p className="titleServices">Services</p>
+            <div className="sectionServices">
+              <div className="itemServices">Restaurantes</div>
+              <div className="itemServices">Hoteles</div>
+              <div className="itemServices">Museos</div>
+              <div className="itemServices">Turismo Aventura</div>
+              <div onClick={() => this.changeState(1, 'Playas y Balnearios')} className="itemServices">
+                Playas y Balnearios
+              </div>
+              <div onClick={() => this.changeState(1, 'Parques y Plazas')}className="itemServices">Parques y Plazas</div>
+              <div className="itemServices">Otros</div>
+          </div>
         );
-    }
+      } else {
+         return <ListServices type={this.state.typePage}/>
+      }
+  }
 }
 
 export default Category;
